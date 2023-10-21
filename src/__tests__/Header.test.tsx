033b1cb3d75331039bd9001ea8e025043a7bf57a
piofr <piotr.ogorkiewicz96@crafton.pl>
2023-10-21 23:03:47 +0200

    first commit

---
+++ src/__tests__/Header.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react'
+import Button from "../components/UI/Button";
+
+const mockData = [
+  {
+    "id": 1,
+    "first_name": "Fletcher",
+    "last_name": "McVanamy",
+    "email": "mmcvanamy0@e-recht24.de",
+    "age": 30
+  }, {
+    "id": 2,
+    "first_name": "Clarice",
+    "last_name": "Harrild",
+    "email": "charrild1@dion.ne.jp",
+    "age": 35
+  },
+]
+
+test("Testing Header component", () => {
+  render(<Button />);
+
+  expect(screen.getByText(/fletcher/i)).toBeInTheDocument();
+})
